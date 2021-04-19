c59a752d3f37b9930ba6b243d5e501306a40ee9f
iuk-ink <87976436@qq.com>
2021-04-20 01:25:10 +0800

    更新

---
+++ conch/asset/admin/hltheme.php
@@ -0,0 +1,344 @@
+<?php
+return array (
+  'theme' => 
+  array (
+    'logo' => 
+    array (
+      'bpic' => 'template/conch/asset/img/logo_hs.png',
+      'wpic' => 'template/conch/asset/img/logo_bs.png',
+      'icon' => 'template/conch/asset/img/favicon.png',
+      'webapp' => 'template/conch/asset/img/ios-fav.png',
+    ),
+    'lazy' => 'template/conch/asset/img/loading.png',
+    'tj' => 
+    array (
+      'btn' => '0',
+    ),
+    'head' => 
+    array (
+      'text' => '',
+    ),
+    'foot' => 
+    array (
+      'text' => '<p>本站只提供WEB页面服务，本站不存储、不制作任何视频，不承担任何由于内容的合法性及健康性所引起的争议和法律责任。</p>
+<p>若本站收录内容侵犯了您的权益，请附说明联系邮箱，本站将第一时间处理。</p>',
+    ),
+    'static' => 
+    array (
+      'url' => 'https://cdn.jsdelivr.net/gh/iuk-ink/iuk@master/conch',
+    ),
+    'type' => 
+    array (
+      'hom' => '1,2,3,4',
+      'meunbtn' => '1',
+      'meunid' => '1,2,3,4,5',
+      'meunys' => 'two',
+      'ht' => '1,2,3,4',
+      'ho' => '999',
+      'hb' => '999',
+      'zb' => '999',
+      'mx' => '999',
+    ),
+    'vod' => 
+    array (
+      'hnum' => '6',
+      'num' => '6',
+    ),
+    'notice' => 
+    array (
+      'btn' => '1',
+      'text' => '<span class=\'mytip\'><font color="#FFFFFF">提示</font></span>最新App端1.1.9已发布<a target="_blank" href="http://app.puz.pub" style="color: #009788">【点击下载】</a>PZ影视 App端浏览速度更快，还有很多独家资源、实用功能等，别忘了收藏本站方便下次浏览！',
+    ),
+    'hotvod' => 
+    array (
+      'btn' => '1',
+      'hb' => '0',
+      'tj' => '0',
+      'title' => '',
+      'num' => 'one',
+    ),
+    'art' => 
+    array (
+      'hbtn' => '1',
+      'htitle' => '热点资讯',
+      'hnum' => '6',
+    ),
+    'topic' => 
+    array (
+      'hbtn' => '1',
+      'hnum' => '6',
+      'title' => '专题',
+      'btn' => '1',
+    ),
+    'actor' => 
+    array (
+      'hbtn' => '1',
+      'htitle' => '荧幕热星',
+      'title' => '明星',
+      'btn' => '1',
+    ),
+    'rank' => 
+    array (
+      'hbtn' => '1',
+      'hby' => 'week',
+      'hid' => '1,2,3,4',
+      'vby' => 'week',
+      'btn' => '1',
+      'title' => '排行榜',
+      'num' => '6',
+      'id' => '1,2,3,4,6,7',
+    ),
+    'links' => 
+    array (
+      'btn' => '1',
+      'title' => '友情链接',
+      'num' => '30',
+    ),
+    'banner' => 
+    array (
+      'btn' => '1',
+      'ms' => 'small',
+      'smbg' => '0',
+      'bgstyle' => '',
+    ),
+    'lbbanner' => 
+    array (
+      'btn' => '0',
+    ),
+    'search' => 
+    array (
+      'text' => '海量影片精彩看不停',
+      'lxbtn' => '1',
+    ),
+    'play' => 
+    array (
+      'height' => '56.25',
+      'adbtn' => '1',
+      'nbtn' => '0',
+      'notice' => '<li>播放器已更换优化，清除浏览器缓存体验更佳。</li>
+<li><span class=\'mytip\'>提醒</span>不要轻易相信视频中的广告，谨防上当受骗!</li>
+<li>如果无法播放请重新刷新页面，或者切换线路。</li>
+<li>视频载入速度跟网速有关，请耐心等待几秒钟。</li>',
+    ),
+    'show' => 
+    array (
+      'filter' => 'a|d|e',
+    ),
+    'playlink' => 
+    array (
+      'btn' => '0',
+    ),
+    'nav' => 
+    array (
+      'id' => '1,2,3,4,5',
+      'zdybtn' => '1',
+      'zdybtn1' => '0',
+      'zdyname1' => '',
+      'zdylink1' => '',
+      'zdypic1' => '',
+      'zdybtn2' => '0',
+      'zdyname2' => '',
+      'zdylink2' => '',
+      'zdypic2' => '',
+      'zdybtn3' => '0',
+      'zdyname3' => '',
+      'zdylink3' => '',
+      'zdypic3' => '',
+      'zdybtn4' => '0',
+      'zdyname4' => '',
+      'zdylink4' => '',
+      'zdypic4' => '',
+    ),
+    'rtnav' => 
+    array (
+      'ym' => 'a|d|f|h',
+      'zdybtn1' => '0',
+      'zdyname1' => '',
+      'zdylink1' => '',
+      'zdybtn2' => '0',
+      'zdyname2' => '',
+      'zdylink2' => '',
+    ),
+    'fnav' => 
+    array (
+      'btn' => '1',
+      'id' => '1,2,3,4',
+      'ym' => 'h|b|h',
+      'zdybtn1' => '0',
+      'zdyname1' => '',
+      'zdylink1' => '',
+      'zdybtn2' => '0',
+      'zdyname2' => '',
+      'zdylink2' => '',
+    ),
+    'share' => 
+    array (
+      'bdbtn' => '1',
+      'zdyfx' => '1',
+      'link' => 'http://www.puz.pub',
+    ),
+    'qq' => 
+    array (
+      'btn' => '0',
+      'title' => '联系QQ',
+      'link' => 'http://wpa.qq.com/msgrd?v=3&uin=87976436&site=qq&menu=yes',
+    ),
+    'weixin' => 
+    array (
+      'btn' => '1',
+      'btntext1' => '关注公众号',
+      'btntext' => '关注“沐宇阁”公众号观影不迷路',
+      'qrcode' => 'template/conch/asset/img/ewm.jpg',
+      'title' => '关注公众号免费看VIP影视',
+      'text' => '<p>长按识别二维码或微信扫码关注</p><p>或微信搜索微信名：<span class=\'mycol\'>沐宇阁</span></p>
+
+',
+    ),
+    'zans' => 
+    array (
+      'btn' => '1',
+      'jjtc' => '就知道你会点，哼~！不过还要祝你观影愉快~!',
+      'qrcode' => 'template/conch/asset/img/dsm.jpg',
+      'title' => '感谢赞赏',
+      'text' => '<p>长按识别二维码或微信扫描二维码</p><p>金额随意，多少都是支持</p>',
+    ),
+    'apps' => 
+    array (
+      'btn' => '1',
+      'link' => 'http://app.puz.pub',
+    ),
+    'map' => 
+    array (
+      'btn' => '1',
+      'title' => '最近更新',
+      'id' => '',
+    ),
+    'color' => 
+    array (
+      'select' => '0',
+      'sbtn' => '0',
+      'ms' => 'white',
+      'mbtn' => '0',
+    ),
+    'role' => 
+    array (
+      'title' => '角色',
+      'btn' => '0',
+    ),
+    'plot' => 
+    array (
+      'title' => '剧情',
+      'btn' => '0',
+    ),
+    'gbook' => 
+    array (
+      'title' => '留言',
+    ),
+    'user' => 
+    array (
+      'title' => '会员',
+    ),
+    'font' => '0',
+    'seos' => 
+    array (
+      'index_name' => '',
+      'index_key' => '',
+      'index_des' => '',
+      'detail_name' => '',
+      'detail_key' => '',
+      'detail_des' => '',
+      'play_name' => '',
+      'play_key' => '',
+      'play_des' => '',
+      'down_name' => '',
+      'down_key' => '',
+      'down_des' => '',
+      'arti_name' => '',
+      'arti_key' => '',
+      'arti_des' => '',
+      'artd_name' => '',
+      'artd_key' => '',
+      'artd_des' => '',
+      'topic_name' => '',
+      'topic_key' => '',
+      'topic_des' => '',
+      'topicd_name' => '',
+      'topicd_key' => '',
+      'topicd_des' => '',
+    ),
+    'ads' => 
+    array (
+      'btn' => '0',
+      'bottom' => 
+      array (
+        'btn' => '0',
+        'content' => '<a href="#"><img src="/template/conch/asset/img/ads-demo.jpg" /></a>',
+      ),
+      'all' => 
+      array (
+        'btn' => '0',
+        'content' => '<a href="#"><img src="/template/conch/asset/img/ads-demo.jpg" /></a>',
+      ),
+      'banner' => 
+      array (
+        'btn' => '0',
+        'tbtn' => '0',
+        'title' => '',
+        'sub' => '',
+        'link' => '',
+        'pic' => '',
+      ),
+      'vod_w' => 
+      array (
+        'btn' => '0',
+        'content' => '<a href="#"><img src="/template/conch/asset/img/ads-demo.jpg" /></a>
+',
+      ),
+      'vod_r' => 
+      array (
+        'btn' => '0',
+        'content' => '<a href="#"><img src="/template/conch/asset/img/ads-demo.jpg" /></a>',
+      ),
+      'play' => 
+      array (
+        'btn' => '0',
+        'tbtn' => '1',
+        'title' => '测试广告',
+        'link' => '',
+        'pic' => '/template/conch/asset/img/ads-demo.jpg',
+      ),
+      'search_v' => 
+      array (
+        'btn' => '0',
+        'tbtn' => '1',
+        'title' => '测试广告',
+        'sub' => '测试广告',
+        'link' => '',
+        'pic' => '/template/conch/asset/img/ads-demo.jpg',
+      ),
+      'art_w' => 
+      array (
+        'btn' => '0',
+        'content' => '<a href="#"><img src="/template/conch/asset/img/ads-demo.jpg" /></a>',
+      ),
+      'art_r' => 
+      array (
+        'btn' => '0',
+        'content' => '<a href="#"><img src="/template/conch/asset/img/ads-demo.jpg" /></a>',
+      ),
+      'artlist' => 
+      array (
+        'btn' => '0',
+        'tbtn' => '1',
+        'title' => '测试广告',
+        'link' => '',
+        'pic' => '/template/conch/asset/img/ads-demo.jpg',
+      ),
+      'user' => 
+      array (
+        'btn' => '0',
+        'pic' => '/template/conch/asset/img/ads-demo.jpg',
+      ),
+    ),
+  ),
+);
