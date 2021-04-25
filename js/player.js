8d3a10b5f2d9b198f3a08d41d57c935c6d4fde0f
iuk-ink <87976436@qq.com>
2021-04-25 19:17:55 +0800

    更新

---
+++ js/player.js
@@ -0,0 +1,28 @@
+console.log(btjson_ver);
+
+if(btjson_ver == "1.0.1"){
+	document.write("<script language=javascript src='https://cdn.jsdelivr.net/gh/btjson/5mrk@0.0.5/js/md5.js'></script>");
+	document.write("<script language=javascript src='https://cdn.jsdelivr.net/gh/btjson/5mrk@0.0.5/js/jquery.min.js'></script>");
+	document.write("<script language=javascript src='https://cdn.jsdelivr.net/gh/btjson/5mrk@0.0.5/js/btjson.min.js'></script>");
+	document.write("<script language=javascript src='https://cdn.jsdelivr.net/gh/btjson/5mrk@0.0.5/btjsonplayer.js'></script>");
+	document.write("<script language=javascript src='https://cdn.jsdelivr.net/gh/btjson/5mrk@0.0.5/js/layer.min.js'></script>");
+}
+
+
+if(btjson_ver == "1.0.0"){
+	document.write("<script language=javascript src='https://cdn.jsdelivr.net/gh/btjson/5mrk@0.0.3/js/md5.js'></script>");
+	document.write("<script language=javascript src='https://cdn.jsdelivr.net/gh/btjson/5mrk@0.0.3/js/jquery.min.js'></script>");
+	document.write("<script language=javascript src='https://cdn.jsdelivr.net/gh/btjson/5mrk@0.0.3/js/btjson.min.js'></script>");
+	document.write("<script language=javascript src='https://cdn.jsdelivr.net/gh/btjson/5mrk@0.0.3/btjsonplayer.js'></script>");
+	document.write("<script language=javascript src='https://cdn.jsdelivr.net/gh/btjson/5mrk@0.0.3/js/layer.min.js'></script>");
+}
+
+
+if(btjson_ver == "0.3.8"){
+
+	document.write("<script language=javascript src='https://cdn.jsdelivr.net/gh/btjson/5mrk@0.0.3/js/md5.js'></script>");
+	document.write("<script language=javascript src='https://cdn.jsdelivr.net/gh/btjson/5mrk@0.0.3/js/jquery.min.js'></script>");
+	document.write("<script language=javascript src='https://cdn.jsdelivr.net/gh/btjson/5mrk@0.0.3/js/btjson.min.js'></script>");
+	document.write("<script language=javascript src='https://cdn.jsdelivr.net/gh/btjson/5mrk@0.0.3/xinbtjsonplayer.min.js'></script>");
+	document.write("<script language=javascript src='https://cdn.jsdelivr.net/gh/btjson/5mrk@0.0.3/js/layer.min.js'></script>");
+}
